635bff3e50e91fc183feeaafec25a127572e70a8
kevindav22 <kevindapit22@gmail.com>
2023-06-18 06:43:55 +0700

    first commit

---
+++ app/Http/Controllers/profileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\metadata;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class profileController extends Controller
+{
+    function index(){
+        return view ('dashboard.profile.index');
+    }
+    function update(Request $request){
+        $request->validate([
+            '_foto' => 'mimes:jpeg,jpg,png,gif',
+            '_email'=> 'required|email'
+        ],[
+            '_foto.mimes' => 'Hanya diperbolehkan mengirim foto berekstensi JPEG, JPG, PNG dan GIF ',
+            '_email.required'=>'Email wajib diisi',
+            '_email.email' => 'Masukkan Format Email yang Valid'
+        ]);
+
+        if($request->hasfile('_foto')){
+            $foto_file = $request->file('_foto');
+            $foto_ekstensi =$foto_file->extension();
+            $foto_baru = date('ymdhis'). ".$foto_ekstensi";
+            $foto_file->move(public_path('foto'), $foto_baru);
+            //untuk update foto
+            $foto_lama = get_meta_value('_foto');
+            File::delete(public_path('foto')."/" .$foto_lama);
+
+            metadata::updateOrCreate(['meta_key'=>'_foto'],['meta_value'=>$foto_baru]);
+        }
+
+        metadata::updateOrCreate(['meta_key'=>'_email'],['meta_value'=>$request->_email]);
+        metadata::updateOrCreate(['meta_key'=>'_kota'],['meta_value'=>$request->_kota]);
+        metadata::updateOrCreate(['meta_key'=>'_provinsi'],['meta_value'=>$request->_provinsi]);
+        metadata::updateOrCreate(['meta_key'=>'_nohp'],['meta_value'=>$request->_nohp]);
+
+        metadata::updateOrCreate(['meta_key'=>'_facebook'],['meta_value'=>$request->_facebook]);
+        metadata::updateOrCreate(['meta_key'=>'_twitter'],['meta_value'=>$request->_twitter]);
+        metadata::updateOrCreate(['meta_key'=>'_linkedin'],['meta_value'=>$request->_linkedin]);
+        metadata::updateOrCreate(['meta_key'=>'_github'],['meta_value'=>$request->_github]);
+
+
+        return redirect()->route('profile.index')->with ('success', 'Berhasil Update data Profile');
+    }
+}
